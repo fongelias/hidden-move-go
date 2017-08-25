e81b5ea05220a0419f388db43d2361cb70ce5fd3
fongelias <fongelias@gmail.com>
2017-08-25 13:56:05 -0400

    Updated dependencies and added routing to index.jsx

--- clientSrc/src/app/index.jsx
+++ clientSrc/src/app/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/containers/app.jsx';
 //Styles
 require('../scss/hiddenMoveGo.scss');
 
 
 ReactDOM.render(
-	<h1>Hello</h1>,
+	(
+		<BrowserRouter>
+			<App/>
+		</BrowserRouter>
+	),
 	document.getElementById("root")
 )
